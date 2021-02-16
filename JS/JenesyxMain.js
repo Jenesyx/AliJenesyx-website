423ad1caf3254b0c73476e2ed79c36b7749d083c
Ali Bidkhori <67784811+Jenesyx@users.noreply.github.com>
2021-02-16 19:07:35 +0100

    1.7 added HTML,CSS,JS skills and socialmedia section changed

--- JS/JenesyxMain.js
+++ JS/JenesyxMain.js
@@ -4,3 +4,32 @@ window.onload = function(){
         else document.getElementById('nav').classList.remove('toggled');
     }
 }
+
+function havij() 
+{
+    document.getElementById('text1').style.left = "290px"
+    document.getElementById('text1').style.bottom = "125px"
+}
+
+function moz() 
+{
+    document.getElementById('text2').style.left = "330px"
+    document.getElementById('text2').style.bottom = "120px"
+}
+
+function sib() 
+{
+    document.getElementById('text3').style.left = "370px"
+    document.getElementById('text3').style.bottom = "120px"
+}
+
+function gilas() 
+{
+    document.getElementById('text4').style.left = "290px"
+    document.getElementById('text4').style.bottom = "125px"
+}
+
+function khiar()
+{
+    document.getElementById('text1')
+}
