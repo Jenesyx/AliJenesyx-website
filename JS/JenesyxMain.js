13e73b2abdaea90451fef16f6d74fd3e5b5c66c1
Ali Bidkhori <a.bidkhori2004@gmail.com>
2021-03-27 14:19:16 +0100

    2.4 - added new codes, new sections and new images

--- JS/JenesyxMain.js
+++ JS/JenesyxMain.js
@@ -1,8 +1,28 @@
-window.onload = function(){
-    document.body.onscroll = function(e){
-        if (document.getElementById('socialmedia').getBoundingClientRect().y < (document.body.clientHeight * 1.5)) document.getElementById('nav').classList.add('toggled')
-        else document.getElementById('nav').classList.remove('toggled');
-    }
+function onload()
+{
+    var typed = new Typed('#typing', 
+    {
+        strings: 
+        [
+            "Jenesyx", "Bidkhori", "Website", 
+            "Your friend", "Your family",
+            "is just kidding"
+        ],
+        typeSpeed: 120,
+        loop: true,
+        startDelay: 2200,
+        backSpeed: 60
+    });
+
+    $('.example-slider').slick(
+    {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplaySpeed: 1500,
+        autoplay: true,
+        centerMode: false,
+        dots: true,
+    });
 }
 
 function banana(t) 
